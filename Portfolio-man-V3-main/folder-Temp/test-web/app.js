e7937087945341de64bff627096e676a6f92ad54
Henrik Storm <89930729+stormlimitless@users.noreply.github.com>
2023-02-09 06:43:44 -0800

    Delete Portfolio-man-V3-main/folder-Temp directory

--- Portfolio-man-V3-main/folder-Temp/test-web/app.js
+++
@@ -1,9 +0,0 @@
-$('#edit').on('click', function(){
-  $('body').addClass('active-modal');
-  $('.box-modal').addClass('modal-show');
-});
-
-$('.overlay').on('click', function(){
-  $('body').removeClass('active-modal');
-  $('.box-modal').removeClass('modal-show');
-})
